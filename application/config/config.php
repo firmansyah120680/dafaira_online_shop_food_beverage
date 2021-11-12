20b4d2a5b4cc4500e7bbdb3e9d44dace0ce30a21
firmansyah120680 <firmansyahgro@gamil.com>
2021-11-12 18:42:59 +0700

    Upload Tugas Project UAS WEB ProgrammingII

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/toko_online_shop_dafaira/';
+$config['base_url'] = 'http://localhost/dafaira_online_shop_food_beverage/';
 
 /*
 |--------------------------------------------------------------------------
